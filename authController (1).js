0078f07f172252a5d9a70a2f1d4898b412e5bdc0
akshitajilagam <akshitajilagam@gmail.com>
2026-03-25 10:11:14 +0530

    Create authController (1).js

---
+++ authController (1).js
@@ -0,0 +1,40 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+/* ---------- SIGN UP ---------- */
+
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // check existing user
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
